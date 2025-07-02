d73592b0c0d5ee4636f6de3bd36124262288e7ad
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-07-03 04:26:01 +0700

    feat: script for copy core-wasm-deploy

---
+++ core-wasm-deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if [ ! -d "./dist/core/wasm" ] ; then
+  echo "[ERROR] folder none - ./dist/core/wasm"
+  exit 1
+fi
+
+if [ ! -d "./dist/core/wasm/huffman-wasm.wasm" ] ; then
+  echo "[ERROR] file 'huffman-wasm.wasm' in folder"
+  exit 1
+fi
+
+cp -r src/core/wasm/huffman-wasm.wasm dist/core/wasm/
